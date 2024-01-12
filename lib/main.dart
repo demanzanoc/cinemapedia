f247ed1f656d605eb7353b8d3a950bf85d449d5b
David Manzano <demanzanoc07@gmail.com>
2024-01-12 18:29:21 -0500

    Se configura variable de entorno

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 
-void main() => runApp(const MainApp());
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
