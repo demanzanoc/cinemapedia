a8c68a607886af4a5658fb72974b314c1ccc0bb3
David Manzano <demanzanoc07@gmail.com>
2024-01-16 16:55:19 -0500

    Se crean modelos, mappers y providers para consumir información de las peliculas

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: moviedb.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
+            : 'https://ih1.redbubble.net/image.1027712254.9762/fposter,small,wall_texture,product,750x1000.u2.jpg',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: moviedb.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+}
---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,7 @@
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Este repositorio es inmutable (solo lectura)
+final movieRepositoryProvider =
+    Provider((ref) => MovieRepositoryImpl(MoviedbDatasource()));
