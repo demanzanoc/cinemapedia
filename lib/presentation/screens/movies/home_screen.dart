d5354fae2220560298b8e394c03bcef542ce53bc
David Manzano <demanzanoc07@gmail.com>
2024-01-17 13:12:29 -0500

    Se crea pagina con appbar para mostrar las peliculas. Se agrega un Swiper para mostrar una lista deslizable horizontal de peliculas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -39,14 +40,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       );
     }
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppBar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
